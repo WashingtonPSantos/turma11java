fe18c005ee6391c42f74bc54029622ce8fb890cb
washington pereira <washington_pereira@hotmail.com>
2020-10-08 00:05:06 -0300

    Out7 git commit -m Out7

---
+++ java/exercicios/src/exercicios/ChefeDeFamilia.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+import java.util.Scanner;
+
+	public class ChefeDeFamilia {
+	public static void main(String[] args) {
+	Scanner leia = new Scanner(System.in); 
+
+	// * chefe de familia
+	//* ler nome e ano de nascimetno  e se é chefe de familia e o sexo * 
+	// * >= 18  idade e for chefe de familia recebe 600 reais
+	//* se for feminino  recebe o dobro
+	
+	String nome ;
+	int anoNascimento ;
+	
+	System.out.print("Qual o seu nome: ");
+	nome = leia.next();
+	System.out.println("Em que ano você nasceu: ");
+	anoNascimento = leia.nextInt();
+	
+	
+	
+	
+	}
+	}
---
+++ java/exercicios/src/portugol/MaiorInteiro.java
@@ -0,0 +1,47 @@
+package portugol;
+
+import java.util.Scanner;
+
+public class MaiorInteiro {
+	public static void main(String[] args) {
+		// 1- Faça um programa que receba três inteiros e diga qual deles é o maior.
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int a,b,c;
+		
+		
+		System.out.print("Digite o valor de a: ");
+		a = leia.nextInt();
+		System.out.print("Digite o valor de b: ");
+		b = leia.nextInt();
+		System.out.print("Digite o valor de c: ");
+		c= leia.nextInt();
+		
+		//System.out.println("O valor de a é: "+a+". O valor de b é: "+b+". O valor de c é:"+c);
+		
+		if (a>=b){
+			//System.out.println("a é maior que b");
+			if	(a>=c) {
+				System.out.println(" a é o maior");
+			}
+		}
+		
+		if (b>=a){
+			//System.out.println("a é maior que b");
+			if	(b>=c) {
+				System.out.println(" b é o maior");
+			}
+		} 
+		
+		if (c>=a){
+			//System.out.println("a é maior que b");
+			if	(c>=b) {
+				System.out.println(" c é o maior");
+			}
+		}	
+		else {
+			System.out.println(" A,b e C são iguais");
+		}
+	}
+}
